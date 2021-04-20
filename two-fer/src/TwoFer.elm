c4e343465f1d26f2b5e859cdcdbdccc904308322
kbayrhammer <klaus.bayrhammer@redbull.com>
2021-04-20 19:31:31 +0200

    feat(two-fer): second iteration

--- two-fer/src/TwoFer.elm
+++ two-fer/src/TwoFer.elm
@@ -3,12 +3,7 @@ module TwoFer exposing (twoFer)
 
 twoFer : Maybe String -> String
 twoFer input =
-    case input of
-        Nothing ->
-            twoFerResponse "you"
-        Just name ->
-            twoFerResponse name
-
-twoFerResponse : String -> String
-twoFerResponse name =
-    "One for " ++  name ++ ", one for me."
+    let
+        name = Maybe.withDefault "you" input
+    in
+        "One for " ++  name ++ ", one for me."
