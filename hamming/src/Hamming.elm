b8ad6f4235a2ea939fc4cfcfcf453030b8907a66
kbayrhammer <klaus.bayrhammer@redbull.com>
2021-04-22 07:48:30 +0200

    feat(hamming): second iteration

--- hamming/src/Hamming.elm
+++ hamming/src/Hamming.elm
@@ -1,17 +1,16 @@
 module Hamming exposing (distance)
 
+import List exposing (filter)
+import String exposing (length, toList)
 
-import List exposing (filter, length)
-import String exposing (toList)
 distance : String -> String -> Result String Int
 distance left right =
     let
-        leftList = toList left
-        rightList = toList right
-        zippedList = List.map2 (\a b -> a == b) leftList rightList
-        hammingDistance = length (filter (\x -> x == False) zippedList)
+        hammingDistance = List.map2 (\a b -> a == b) (toList left) (toList right)
+           |> filter (\x -> x == False)
+           |> List.length
     in
-        if not (length rightList == length leftList) then
+        if not (length right == length left) then
             Err "left and right strands must be of equal length"
         else
             Ok hammingDistance
