700bcc284a5ffa3f79db915ba8e5e2b37cac51ac
kbayrhammer <klaus.bayrhammer@redbull.com>
2021-04-23 19:39:10 +0200

    refactor(sum-of-multiples): second iteration

--- sum-of-multiples/src/SumOfMultiples.elm
+++ sum-of-multiples/src/SumOfMultiples.elm
@@ -1,12 +1,12 @@
 module SumOfMultiples exposing (sumOfMultiples)
 
-import List exposing (any, filter, range, sum)
 
 sumOfMultiples : List Int -> Int -> Int
 sumOfMultiples divisors limit =
     let
-        dividableByAnyDivisor element = any (\divisor -> modBy divisor element == 0) divisors
+        dividableByAnyDivisor element =
+            List.any (\divisor -> modBy divisor element == 0) divisors
     in
-        range 1 (limit - 1)
-        |> filter dividableByAnyDivisor
-        |> sum
+    List.range 1 (limit - 1)
+        |> List.filter dividableByAnyDivisor
+        |> List.sum
