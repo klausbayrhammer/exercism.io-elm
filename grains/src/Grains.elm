1a4c78f3f2f0a3c5914c64d0e3abef30e3c102d0
kbayrhammer <klaus.bayrhammer@redbull.com>
2021-04-26 19:14:23 +0200

    feat(grains): second iteration

--- grains/src/Grains.elm
+++ grains/src/Grains.elm
@@ -1,21 +1,10 @@
 module Grains exposing (square)
 
 
-squareGrains : Int -> Int
-squareGrains n =
-    case n of
-        1 ->
-            1
-
-        _ ->
-            2 * squareGrains (n - 1)
-
-
 square : Int -> Maybe Int
 square n =
     if n <= 0 then
         Nothing
 
     else
-        squareGrains n
-            |> Just
+        Just (2 ^ (n - 1))
